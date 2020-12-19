c4743d4e9c8e99d711071c26719ed089df529369
Matthew O <mo@pop-os.localdomain>
2020-12-19 13:12:15 -0700

    basic nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Nav />
     </div>
   );
 }
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,1 +1,33 @@
 import React from "react";
+import { FaFolderOpen } from "react-icons/fa";
+import { IoIosContact } from "react-icons/io";
+import { SiAboutDotMe } from "react-icons/si";
+
+function Nav() {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a href="#" className="nav-link">
+            <SiAboutDotMe size={30} color="white" />
+            <span className="link-text">About</span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="#" className="nav-link">
+            <FaFolderOpen size={30} color="white" />
+            <span className="link-text">Projects</span>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="#" className="nav-link">
+            <IoIosContact size={30} color="white" />
+            <span className="link-text">Contact</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
