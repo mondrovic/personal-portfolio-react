85bb044edd6faa65d1ffef01b78d9c319fc3c74f
Matthew O <mo@pop-os.localdomain>
2020-12-20 17:03:18 -0700

    footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Nav from "./components/Nav";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Projects from "./components/Projects";
+import Footer from "./components/Footer";
 
 function App() {
   const [menus] = useState([
@@ -14,20 +15,18 @@ function App() {
   const [menuSelected, setMenuSelected] = useState(menus[0].name);
 
   return (
-    <div>
-      <header>
-        <Nav
-          menus={menus}
-          menuSelected={menuSelected}
-          setMenuSelected={setMenuSelected}
-        />
-      </header>
+    <div className="d-flex flex-column">
+      <Nav
+        menus={menus}
+        menuSelected={menuSelected}
+        setMenuSelected={setMenuSelected}
+      />
       <main>
         {menuSelected === "about" && <About />}
         {menuSelected === "contact" && <Contact />}
         {menuSelected === "projects" && <Projects />}
       </main>
-      <footer></footer>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="bg-light text-center text-lg-start">
+      <div className="p-3">
+        ©2020 Copyright:
+        <a className="text-dark" href="https://github.com/mondrovic">
+          Matthew Ondrovic
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
